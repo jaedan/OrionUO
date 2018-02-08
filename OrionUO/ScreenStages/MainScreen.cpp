bb785d08e8f2fd713af9fc5f996edc44a93aa03b
Jaedan <jaedan@none.com>
2018-02-08 09:01:21 -0700

    Rename uo_debug.cfg to client.cfg
    This is a clearer name.

--- OrionUO/ScreenStages/MainScreen.cpp
+++ OrionUO/ScreenStages/MainScreen.cpp
@@ -226,7 +226,7 @@ void CMainScreen::LoadCustomPath()
 {
 	WISPFUN_DEBUG("c165_f14");
 
-	WISP_FILE::CTextFileParser file(g_App.ExeFilePath("uo_debug.cfg"), "=", "#;", "");
+	WISP_FILE::CTextFileParser file(g_App.ExeFilePath("client.cfg"), "=", "#;", "");
 
 	while (!file.IsEOF())
 	{
@@ -258,7 +258,7 @@ void CMainScreen::LoadGlobalConfig()
 	m_AutoLogin->Checked = false;
 	g_ScreenEffectManager.Enabled = false;
 
-	WISP_FILE::CTextFileParser file(g_App.ExeFilePath("uo_debug.cfg"), "=", "#;", "");
+	WISP_FILE::CTextFileParser file(g_App.ExeFilePath("client.cfg"), "=", "#;", "");
 
 	while (!file.IsEOF())
 	{
@@ -357,7 +357,7 @@ void CMainScreen::SaveGlobalConfig()
 {
 	WISPFUN_DEBUG("c165_f11");
 	FILE *uo_cfg = NULL;
-	fopen_s(&uo_cfg, g_App.ExeFilePath("uo_debug.cfg").c_str(), "w");
+	fopen_s(&uo_cfg, g_App.ExeFilePath("client.cfg").c_str(), "w");
 
 	if (uo_cfg == NULL)
 		return;
