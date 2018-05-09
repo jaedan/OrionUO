b98d9db8ead299be4774d9a23814f6c541442f55
Jaedan <jaedan@none.com>
2018-05-08 22:42:27 -0700

    Remove command line arguments from OrionUO.exe

--- OrionUO/OrionUO.h
+++ OrionUO/OrionUO.h
@@ -107,8 +107,6 @@ private:
 
 	string DecodeArgumentString(const char *text, int length);
 
-	void ParseCommandLine();
-
 	void LoadPlugin(const string &libpath, const string &function, int flags);
 
 	void LoadContaierOffsets();
