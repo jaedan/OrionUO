62e6bacc6dd137d1c3dbbb1347f614a94ee4a0f6
Jaedan <jaedan@none.com>
2018-06-12 23:35:18 -0700

    Add a script to run clang-format on the whole repository.
    It also catches those crazy //---- line break comments.

---
+++ scripts/check_format.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+readonly BASEDIR=$(readlink -f $(dirname $0))/..
+cd $BASEDIR
+
+# exit on errors
+set -e
+
+rc=0
+
+if hash clang-format; then
+	echo -n "Checking coding style..."
+	rm -f format.log
+	touch format.log
+	git ls-files '*.[ch]' '*.cpp' '*.cc' '*.cxx' '*.hh' '*.hpp' ':!OrionUO/Resource.h' ':!*Dependencies/*' | \
+		xargs -n8 -P8 clang-format -i -style=file >> format.log
+	if [ -s format.log ]; then
+		echo " errors detected"
+		git diff
+		echo "Incorrect code style detected in one or more files."
+		echo "The files have been automatically formatted."
+		echo "Remember to add the files to your commit."
+		rc=1
+	else
+		echo " OK"
+	fi
+	rm -f format.log
+else
+	echo "You do not have clang-format installed so your code style is not being checked!"
+fi
+
+echo -n "Checking comment style..."
+git grep --line-number -e '//---' -- '*.cpp' '*.h' > comment.log || true
+if [ -s comment.log ]; then
+	echo " Line break fillers detected."
+	cat comment.log
+	rc=1
+else
+	echo " OK"
+fi
+rm -f comment.log
+
+exit $rc
