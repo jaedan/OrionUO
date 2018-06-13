147c2bd52aa082141f12c7e43603caf54032d6f6
Jaedan <jaedan@none.com>
2018-06-12 23:45:55 -0700

    Remove all PVS-Studio comments

--- OrionUO/Managers/FileManager.cpp
+++ OrionUO/Managers/FileManager.cpp
@@ -1,5 +1,3 @@
-﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 /***********************************************************************************
 **
 ** FileManager.cpp
